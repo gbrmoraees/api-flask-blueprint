0d5a0d73d2b23e60d96ea8e960e05398f5e235c2
carlosrmfernandes <carlosr.m.fernandes@hotmail.com>
2024-08-20 11:36:04 -0300

    update project

--- path/to/venv/app/config/config.py
+++ path/to/venv/app/config/config.py
@@ -1,10 +1,16 @@
 import mysql.connector
 
 def get_db_connection():
+    """
+    Estabelece uma conexão com o banco de dados MySQL.
+
+    Retorna:
+    - Uma conexão com o banco de dados MySQL, configurada com os parâmetros especificados.
+    """
     return mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='root',
-        database='lp_trainer',
-        port=8889
+        host='127.0.0.1',         # Endereço do servidor MySQL. '127.0.0.1' refere-se ao servidor local.
+        user='root',             # Nome de usuário para autenticação no banco de dados.
+        password='root',         # Senha do usuário para autenticação no banco de dados.
+        database='lp_trainer',   # Nome do banco de dados ao qual conectar.
+        port=8889                # Porta na qual o servidor MySQL está ouvindo (porta padrão do MAMP).
     )
